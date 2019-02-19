6305078a0da8187f1e8185c4d1a111332f84ec71
Patrick Kim <pkim050@gmail.com>
2019-02-19 05:29:29 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,6 @@
 class Book
-  attr_accessor :author, :page_count
+  attr_accessor :author, :page_count, :title, :turn_page
   def initialize (title)
     @title = title
   end
-  
-  def title
-    @title
-  end
-
 end
