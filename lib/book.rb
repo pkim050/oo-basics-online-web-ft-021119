4fac166ebbed4430fdfa5ee488b74ece8bf088a4
Patrick Kim <pkim050@gmail.com>
2019-02-19 05:33:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,18 @@
 class Book
   attr_accessor :author, :page_count, :title, :turn_page
-  def initialize (title)
+  def initialize(title)
     @title = title
+  end
+  
+  def genre=(string)
+    @string = string
+  end
+  
+  def genre
+    @string
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
 end
