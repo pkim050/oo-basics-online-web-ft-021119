186aabc807dd8aa40c3becc3ccbe719250fb5b6c
Patrick Kim <pkim050@gmail.com>
2019-02-19 05:39:20 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,10 @@
-
-
+class Book
+  attr_accessor :author, :page_count, :title, :turn_page, :genre
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
